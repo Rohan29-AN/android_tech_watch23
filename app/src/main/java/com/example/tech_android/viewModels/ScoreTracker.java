13b11384e67025487979dc5e937d9400e866f337
Tafita29 <rohanrc101@gmail.com>
2023-06-02 10:27:12 +0300

    build : adding dataBinding

---
+++ app/src/main/java/com/example/tech_android/viewModels/ScoreTracker.java
@@ -0,0 +1,16 @@
+package com.example.tech_android.viewModels;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tech_android.R;
+
+public class ScoreTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_tracker);
+    }
+}
