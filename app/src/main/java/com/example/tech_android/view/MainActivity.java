8910d4f35de744bed5254aebb27d3ebc2908b663
Tafita29 <rohanrc101@gmail.com>
2023-06-02 07:55:30 +0300

    chore: move MainActivity.java to view

--- app/src/main/java/com/example/tech_android/view/MainActivity.java
+++ app/src/main/java/com/example/tech_android/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.tech_android;
+package com.example.tech_android.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.tech_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
